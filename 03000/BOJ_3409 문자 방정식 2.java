1a55626771a249f2623858dc1ccd0c213c1c2d2e
Octoping <myc1365@naver.com>
2022-01-21 22:33:34 +0900

    자바로 구현 완료했는데도 시간초과나네

--- 03000/BOJ_3409 문자 방정식 2.java
+++ 03000/BOJ_3409 문자 방정식 2.java
@@ -2,9 +2,12 @@
 import java.io.*;
 import java.util.*;
 
-
-public class main
+public class Main
 {
+	static String[][] arr;
+	static String T;
+	static String P;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int testcaseCnt = Integer.parseInt(br.readLine());
@@ -25,7 +28,6 @@ public class main
 		        if(tmp[1].indexOf('+') != -1)
 				{
 					String[] tmp2 = tmp[1].split(" \\+ ");
-					value[i][0] = "0";
 					value[i][1] = tmp2[0];
 					value[i][2] = tmp2[1];
 				}
@@ -38,12 +40,12 @@ public class main
 			// 	System.out.println(value[i][0] + " " + value[i][1] + " " + value[i][2]);
 
 
-			// 트리 연결 구조 구하는 2차원 배열 만들기
-			String[][] arr = new String[K][3];
+			// 트리 연결 구조 2차원 배열 만들기
+			arr = new String[K][3];
 
 			for(int i = 0; i < K; ++i)
 			{
-				if(value[i][0] == "0") {
+				if(value[i][0] == null) {
 					arr[i][1] = Integer.toString(varNames.indexOf(value[i][1]));
 					arr[i][2] = Integer.toString(varNames.indexOf(value[i][2]));
 				}
@@ -52,20 +54,49 @@ public class main
 				}
 			}
 
-			// System.out.println('\n');
-			// for(int i = 0; i < K; ++i)
-			// 	System.out.println(arr[i][0] + " " + arr[i][1] + " " + arr[i][2]);
-			
+// 			System.out.println('\n');
+// 			for(int i = 0; i < K; ++i)
+// 				System.out.println(arr[i][0] + " " + arr[i][1] + " " + arr[i][2]);
+
 
 			// 트리 탐색하며 P 구하기
-			String T = br.readLine();
-			String P = br.readLine();
+			T = br.readLine();
+			P = br.readLine();
 
 			int l = recursive(varNames.indexOf(T), 0);
-			if(l == P.length())
-			{
-				
-			}
+
+			System.out.println(l == P.length() ? "YES" : "NO");
 		}
+
+		return;
+	}
+
+	public static int recursive(int nowNode, int readCnt)
+	{
+		if(readCnt == P.length())
+			return readCnt;
+
+		String WORD = arr[nowNode][0];
+
+		if(WORD != null)
+		{
+			int nowReadCnt = 0;
+
+			for(int i = 0; i < WORD.length(); ++i)
+			{
+				if(readCnt + nowReadCnt == P.length())
+					return readCnt + nowReadCnt;
+				
+				if(P.charAt(readCnt + nowReadCnt) == WORD.charAt(i))
+					++nowReadCnt;
+			}
+
+			return readCnt + nowReadCnt;
+		}
+
+		int left = recursive(Integer.parseInt(arr[nowNode][1]), readCnt);
+		int right = recursive(Integer.parseInt(arr[nowNode][2]), left);
+		return right;
 	}
 }
+
