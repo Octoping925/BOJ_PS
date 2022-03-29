5240c3c200d1fbf990d66118a04c291cfbd40b43
Octoping925 <mooner925@gmail.com>
2022-03-29 13:37:45 +0900

    foreach로 수정

--- Programmers/프로그래머스_위장.java
+++ Programmers/프로그래머스_위장.java
@@ -1,22 +1,21 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42578
 import java.util.*;
-import java.util.Map.Entry;
 
 class Solution {
+    int answer = 1;
+    
     public int solution(String[][] clothes) {
         HashMap<String,Integer> hm = new HashMap<>();
         for (String[] clothe : clothes) {
             if (hm.containsKey(clothe[1])) {
                 hm.put(clothe[1], hm.get(clothe[1]) + 1);
-            } else {
-                hm.put(clothe[1], 1);
             }
+            else hm.put(clothe[1], 1);
         }
 
-        int answer = 1;
-        for (Entry<String, Integer> entrySet : hm.entrySet()) {
-            answer *= entrySet.getValue() + 1;
-        }
+        hm.forEach((key, value) -> {
+            answer *= value+1;
+        });
 
         return answer - 1;
     }
