d2d2bb1bd420fa6d08964823382df1c39653d402
Octoping925 <mooner925@gmail.com>
2022-05-31 14:55:04 +0900

    소트인사이드 스트림으로 다시 풀기

--- 01000/BOJ_1427 소트인사이드.java
+++ 01000/BOJ_1427 소트인사이드.java
@@ -6,20 +6,8 @@ public class Main
 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String n = br.readLine();
-        ArrayList<Character> k = new ArrayList<>();
-        
-        for(char i : n.toCharArray()) {
-            k.add(i);
-        }
-        
-        Collections.sort(k, Collections.reverseOrder());
-        
-        for(char i : k) {
-            System.out.print(i);
-        }
-        
-        return;
+        Arrays.stream(br.readLine().split(""))
+            .sorted(Comparator.reverseOrder())
+            .forEach(System.out::print);
     }
 }
