7890187cf869b146ad2241c74c20d38001e263aa
Octoping925 <mooner925@gmail.com>
2022-06-14 07:43:36 +0900

    노솔브 방지문제야 숏코딩

--- 15000/BOJ_15917 노솔브 방지문제야!!.c
+++ 15000/BOJ_15917 노솔브 방지문제야!!.c
@@ -1,10 +1,9 @@
 // https://www.acmicpc.net/problem/15917
 #include <stdio.h>
 int n, q;
-int main()
-{
+int main() {
     scanf("%d", &n);
-    for(int i = 0; i < n; ++i) {
+    while(n--) {
         scanf("%d", &q);
         printf("%d\n", (q&(-q))==q);
     }
