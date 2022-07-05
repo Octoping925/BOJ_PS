e4d761849f6fc31faf9b1635f9c05fc01d29c029
Octoping925 <mooner925@gmail.com>
2022-07-05 14:52:30 +0900

    해석 추가

--- 01000/BOJ_1834 나머지와 몫이 같은 수.c
+++ 01000/BOJ_1834 나머지와 몫이 같은 수.c
@@ -8,3 +8,13 @@ int main()
     scanf("%d", &n);
     printf("%lld", (long long) (n+1) * n * (n-1) / 2);
 }
+
+/*
+X / N = K . . . K
+
+k*n + k  =  k*(n+1) = X
+
+0 <= k < n
+
+(n+1) * (n * (n-1) / 2)
+*/
