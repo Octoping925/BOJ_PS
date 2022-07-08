69ce429276ccfd13694df3b92f3418da3f9c8d77
Octoping925 <mooner925@gmail.com>
2022-07-08 10:31:45 +0900

    인생 점수 스트림으로 리팩토링

--- 15000/BOJ_15351 인생 점수.java
+++ 15000/BOJ_15351 인생 점수.java
@@ -1,20 +1,16 @@
+// https://www.acmicpc.net/problem/15351
 import java.io.*;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
         while(n-->0){
-            int score = 0;
-            char[] str = br.readLine().toCharArray();
-
-            for(char c : str) {
-                if(c != ' ') {
-                    score += c - 'A' + 1;
-                }
-            }
+            int score = br.readLine().chars()
+                .filter(x-> x != ' ')
+                .map(x -> x-'A'+1)
+                .sum();
 
             System.out.println(score == 100 ? "PERFECT LIFE" : Integer.toString(score));
         }
