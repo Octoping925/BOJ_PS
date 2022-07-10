8e52dd2d98bb52b74dfa3113fcfb37861d04c153
Octoping <myc1365@naver.com>
2022-07-10 23:50:45 +0900

    나는 친구가 적다 (Small) 스트림에서 regex로 변경

--- 16000/BOJ_16171 나는 친구가 적다 (Small).java
+++ 16000/BOJ_16171 나는 친구가 적다 (Small).java
@@ -1,15 +1,12 @@
 // https://www.acmicpc.net/problem/16171
+// https://www.acmicpc.net/problem/16171
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String txt = br.readLine().chars()
-                .filter(x -> x > '9')
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
-
+        String txt = br.readLine().replaceAll("\\d", "");
         String original = br.readLine();
 
         System.out.println(txt.contains(original) ? 1 : 0);
