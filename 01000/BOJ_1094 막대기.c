6615aacc66f5781375f6e2a58cc298460601fceb
Octoping925 <mooner925@gmail.com>
2022-12-22 18:55:11 +0900

    막대기

---
+++ 01000/BOJ_1094 막대기.c
@@ -0,0 +1,18 @@
+// https://www.acmicpc.net/problem/1094
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int cnt = 0;
+    for(int i = 1; i <= 64; i <<= 1) {
+        if((n & i) > 0) {
+            cnt++;
+        }
+    }
+
+    printf("%d", cnt);
+    return 0;
+}
