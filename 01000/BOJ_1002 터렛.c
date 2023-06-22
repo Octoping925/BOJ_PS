ff6aed128c64df226820c93055e2f2b35f4d62b2
Octoping <mooner925@gmail.com>
2023-06-22 09:05:50 +0900

    터렛

--- 01000/BOJ_1002 터렛.c
+++ 01000/BOJ_1002 터렛.c
@@ -3,28 +3,26 @@
 
 int n, x1, y1, r1, x2, y2, r2, distance;
 
-int main()
-{
+int main() {
     scanf("%d", &n);
-    for (int i = 1; i <= n; ++i)
-    {
+    while(n--) {
         scanf("%d %d %d %d %d %d", &x1, &y1, &r1, &x2, &y2, &r2);
-        if (x1 == x2 && y1 == y2)
-        {
-            if (r1 == r2) printf("-1");
+
+        if (x1 == x2 && y1 == y2) {
+            if (r1 == r2) {
+                if(!r1) printf("1");
+                else printf("-1");
+            }
             if (r1 != r2) printf("0");
         }
-        else
-        {
+        else {
             distance = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
             if ((r1 + r2) * (r1 + r2) == distance) printf("1");
             else if ((r1 + r2) * (r1 + r2) < distance) printf("0");
             else if ((r1 - r2) * (r1 - r2) == distance) printf("1");
             else if ((r1 - r2) * (r1 - r2) > distance) printf("0");
-            else if ((r1 + r2) * (r1 + r2) > distance) printf("2");  
+            else if ((r1 + r2) * (r1 + r2) > distance) printf("2");
         }
         printf("\n");
     }
-
-    return 0;
 }
